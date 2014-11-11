e1dfb23d8a53a9065b45bb074e90a6a22c351846
Zack Boehm <zacknut@gmail.com>
2014-11-11 16:49:46 -0500

    document

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,41 +1,50 @@
-// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
-
-// var settings = new Store("settings", {
-//     "sample_setting": "This is how you use Store.js to remember values"
-// });
+//TODO:
+// Send 'browsing' status to popup, don't want to spawn multiple instances
+// General cleanup of code & ui
 
 browsing = false;
-sites = [];
 end = new Date();
+sites = [];
+$.getJSON( "../../topsites.json", function( data ) {
+  $.each(data, function(key, val){sites.push(val);});
+});
 
-//example of using a message handler from the inject scripts
 chrome.extension.onMessage.addListener(
   function(request, sender, sendResponse) {
 
-  start = new Date();
-  end = new Date(start.getTime() + request.mins*60000);
+    // Super quick 'error' handling.
+    //TODO: validation in popup script before message send
+    if(!Number.isInteger(request.mins)) chrome.runtime.reload();
 
-  console.log('Starting Auto Browsing');
-  console.log('Complete time is '+request.mins+' minutes from now, at \n'+end);
-  if(request.uninstall) console.log('Uninstalling self upon completion.');
+    //TODO: Inform popup we're doing stuff
+    browsing = true;
 
+    // Grab the start date for history cleaning later on
+    start = new Date();
+    // Calculate the end time from the popup
+    end = new Date(start.getTime() + request.mins*60000);
+  
+    console.log('Starting Auto Browsing');
+    console.log('Complete time is '+request.mins+' minutes from now, at \n'+end);
+    if(request.uninstall) console.log('Uninstalling self upon completion.');
+  
 
-
-  $.getJSON( "../../topsites.json", function( data ) {
-    $.each(data, function(key, val){sites.push(val);});
-  });
-
-  chrome.tabs.create({active: false}, function(tab) {
+    // Create tab, pop it out, and focus it
+    chrome.tabs.create({active: false}, function(tab) {
       chrome.windows.create({tabId: tab.id, type: 'popup', focused: true});
 
+      //TODO: Pull up a local page w/ short explanation for first load
       chrome.tabs.update(tab.id, {url:"http://google.com"});
-
-      browsing = true;
+      
+      // If browsing and end date has not been reached, load a new page every 10 seconds
       window.setInterval(function(){
         if(browsing && end > new Date()){
+          // Update the tab with a new, random page
           chrome.tabs.update(tab.id, {url: "http://"+sites[Math.floor(Math.random()*sites.length)]}, function(){
             if (chrome.runtime.lastError) {
-              console.log(chrome.runtime.lastError.message);
+              console.log('Error when trying to update tab:\n'+chrome.runtime.lastError.message);
+
+              // If error is because tab cannot be found, pop an alert and reload the extension
               re = /(No tab with id:\s\d{0,}.{0,})/;
               if(re.test(chrome.runtime.lastError.message)){
                 browsing = false;
@@ -45,6 +54,9 @@ chrome.extension.onMessage.addListener(
             }
           });
         }
+
+        // We've either manually stopped browsing or have reached the end date.
+        // Cleanup: by killing the tab, deleting history, and uninstalling ourselves. Reload if not uninstalling
         else {
           browsing = false;
           console.log('We\'re done here.');
@@ -54,7 +66,6 @@ chrome.extension.onMessage.addListener(
           chrome.runtime.reload();
         }
       }, 10000);
-  });
+    });
 
-  sendResponse();
 });
--- src/browser_action/browser_action.js
+++ src/browser_action/browser_action.js
@@ -1,16 +1,14 @@
+//TODO: Get browsing state from background, hide UI if browsing
 document.addEventListener('DOMContentLoaded', function() {
 
   $('#go').on('click', function(){
     beginBrowse($('input[type="number"]').val(), $('input[type="checkbox"]').prop("checked"));
   });
-  console.log('DOMContentLoaded');
 
 });
 
-
 function beginBrowse(mins, uninstall){
-  chrome.extension.sendMessage({mins: mins, uninstall: uninstall},
-        function (response) {
-            console.log('response from background'+response);
-        });
+  chrome.extension.sendMessage({mins: mins, uninstall: uninstall}, function (response) {
+    console.log('response from background'+response);
+  });
 }
